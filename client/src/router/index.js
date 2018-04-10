a7fce21c3f04a111543056100d9428155237f4c0
Tyler-Williams <github@tylerclarkwilliams.com>
2018-04-10 09:50:26 -0700

    Skeleton comeplete

---
+++ client/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Products from '../components/Products'
+import Product from '../components/Product'
+import Checkout from '../components/Checkout'
+import Category from '../components/Category.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Products
+    },
+    {
+      path: '/product/:id',
+      name: 'product',
+      component: Product,
+      props: true
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: Checkout
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: Category
+    }
+  ],
+  mode: 'history'
+})
